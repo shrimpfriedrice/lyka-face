9400681a4ccbc97f3fd630ca5d5d22eac5513589
shrimpfriedrice <cookiefriedrice@gmail.com>
2016-08-12 23:23:31 +0530

    Add some images

--- openface-simple-compare/openface-simple-compare.py
+++ openface-simple-compare/openface-simple-compare.py
@@ -6,3 +6,45 @@ import os
 import cv2
 #https://github.com/cmusatyalab/openface
 import openface
+import time
+import itertools
+
+base_path = os.path.dirname(os.path.realpath(__file__))
+dlib_face_predictor_model_path = os.path.join(base_path, 'models/dlib/shape_predictor_68_face_landmarks.dat')
+openface_network_model_path = os.path.join(base_path, 'models/openface/nn4.small2.v1.t7')
+
+img_dimension = 96
+
+start_time = time.time()
+align = openface.AlignDlib(dlib_face_predictor_model_path)
+network = openface.TorchNeuralNet(openface_network_model_path, img_dimensions)
+print ("loading align and network model took {} seconds".format(time.time() - start_time))
+
+#get representation by feeding img into neural net
+def openface_network_forward_pass(img_path):
+
+    img_bgr = cv2.imread(img_path)
+    if img_bgr is None:
+        raise Exception("Unable to load image {}".format(img_path))
+    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+
+    start_time = time.time()
+    face_bounding_box = align.getLargestFaceBoundingBox(img_rgb)
+    if face_bounding_box is None:
+        raise Exception("Unable to find face in image {}".format(img_path))
+    print ("found face in {} in {} seconds").format(img_path, time.time() - start_time)
+
+    start_time = time.time()
+    aligned_face = align.align(img_dimension, img_rgb, face_bounding_box, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+    if aligned_face is None:
+        raise Exception("Unable to align face in image {}".format(img_path))
+    print ("aligned face in {} in {} seconds").format(img_path, time.time() - start_time)
+
+    start_time = time.time()
+    representation = network.forward(aligned_face)
+    print ("openface forward pass took {} seconds".format(time.time()-start_time))
+    return representation
+
+#for (image_1, image_2) in itertools.combinations()
+
+
