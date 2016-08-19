d99aad26e78c7abf6079996ac52d4ec5bc40f11d
shrimpfriedrice <cookiefriedrice@gmail.com>
2016-08-19 16:27:28 +0530

    Added complete classification for simple-classify

--- openface-simple-classify/openface-simple-classify.py
+++ openface-simple-classify/openface-simple-classify.py
@@ -1,7 +1,10 @@
 import os
+import glob
+import time
 import shlex
-import subprocess
 import pickle
+import subprocess
+import numpy as np
 import pandas as pd
 from operator import itemgetter
 
@@ -19,8 +22,10 @@ from openface.data import iterImgs
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 raw_faces_path = os.path.join(base_path, 'images/raw')
+faces_dump_path = os.path.join(base_path, 'images/dump')
 aligned_faces_path = os.path.join(base_path, 'images/aligned')
 dlib_face_predictor = os.path.join(base_path, 'models/dlib/shape_predictor_68_face_landmarks.dat')
+openface_torch_network = os.path.join(base_path, 'models/openface/nn4.small2.v1.t7')
 generate_representations_cache = os.path.join(base_path, 'images/aligned/cache.t7')
 generated_representation_labels = os.path.join(base_path, 'features/labels.csv')
 generated_representation_representations = os.path.join(base_path, 'features/reps.csv')
@@ -87,8 +92,62 @@ def train_classifier():
     with open(classifier_pickle, 'w') as f:
         pickle.dump((label_encoder, clf), f)
 
+
+def infer_classifier():
+    with open(classifier_pickle, 'r') as f:
+        (le, clf) = pickle.load(f)
+
+    images = glob.glob(os.path.join(faces_dump_path, '*.*'))
+    align = openface.AlignDlib(dlib_face_predictor)
+    net = openface.TorchNeuralNet(openface_torch_network, aligned_image_size)
+
+    for img in images:
+        print("\n=== {} ===".format(img))
+        rep = getRep(img, align, net).reshape(1, -1)
+        start = time.time()
+        predictions = clf.predict_proba(rep).ravel()
+        maxI = np.argmax(predictions)
+        person = le.inverse_transform(maxI)
+        confidence = predictions[maxI]
+        print("Prediction took {} seconds.".format(time.time() - start))
+        print("Predict {} with {:.2f} confidence.".format(person, confidence))
+        # if isinstance(clf, GMM):
+        #     dist = np.linalg.norm(rep - clf.means_[maxI])
+        #     print("  + Distance from the mean: {}".format(dist))
+
+
+def getRep(imgPath, align, net):
+    start = time.time()
+    bgrImg = cv2.imread(imgPath)
+    if bgrImg is None:
+        raise Exception("Unable to load image: {}".format(imgPath))
+
+    rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
+
+    print("  + Original size: {}".format(rgbImg.shape))
+    print("Loading the image took {} seconds.".format(time.time() - start))
+
+    start = time.time()
+
+    bb = align.getLargestFaceBoundingBox(rgbImg)
+    if bb is None:
+        raise Exception("Unable to find a face: {}".format(imgPath))
+    print("Face detection took {} seconds.".format(time.time() - start))
+
+    start = time.time()
+    alignedFace = align.align(aligned_image_size, rgbImg, bb,
+                              landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+    if alignedFace is None:
+        raise Exception("Unable to align image: {}".format(imgPath))
+    print("Alignment took {} seconds.".format(time.time() - start))
+
+    start = time.time()
+    rep = net.forward(alignedFace)
+    print("Neural network forward pass took {} seconds.".format(time.time() - start))
+    return rep
+
 align_faces_dlib()
 generate_representations_openface()
 train_classifier()
 
-
+infer_classifier()
